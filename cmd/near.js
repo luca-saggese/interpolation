ae46e3ed20fbce3e7c53de8d57652e444c58674b
Luca <luca.saggese@gmail.com>
2019-07-19 10:13:25 +0200

    Update near.js

--- cmd/near.js
+++ cmd/near.js
@@ -35,7 +35,7 @@ console.log(res)
     res=res.map(i=>{
       var street = i.street;
       street.coordinates= street.coordinates[0] ;
-      street.distance = Math.round(i.proj.dist*100000)/100000;
+      street.distance = Math.round(i.proj.dist*1000000)/1000;
       delete street.line;
       return street;
     });
