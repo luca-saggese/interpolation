676846e5728f2ce6ff930a8e557518a8b15ef80c
Luca <luca.saggese@gmail.com>
2019-07-19 11:41:44 +0200

    Create autocomplete.js

---
+++ query/autocomplete.js
@@ -0,0 +1,50 @@
+
+// maximum names to match on
+var MAX_NAMES = 10;
+
+// maximum address records to return
+var MAX_MATCHES = 20;
+
+/**
+  this query should only ever return max 3 rows.
+  note: the amount of rows returned does not adequently indicate whether an
+  exact match was found or not.
+**/
+
+var SQL = [
+  'SELECT id, name',
+  'FROM street.names',
+  'WHERE ( %%NAME_CONDITIONS%% )',
+  'LIMIT %%MAX_MATCHES%%;'
+].join(' ');
+
+var NAME_SQL = '(street.names.name like ?)';
+
+module.exports = function( db, point, number, names, cb ){
+
+  // error checking
+  if( !names || !names.length ){
+    return cb( null, [] );
+  }
+
+  // max conditions to search on
+  var max = { names: Math.min( names.length, MAX_NAMES ) };
+
+  // use named parameters to avoid sending coordinates twice for rtree conditions
+  var position = 1; // 1 and 2 are used by lon and lat.
+
+  // add name conditions to query
+  var nameConditions = Array.apply(null, new Array(max.names)).map( function(){
+    return NAME_SQL.replace('?', '?' + position++);
+  });
+
+  // build unique sql statement
+  var sql = SQL.replace( '%%NAME_CONDITIONS%%', nameConditions.join(' OR ') )
+               .replace( '%%MAX_MATCHES%%', MAX_MATCHES );
+
+  // create a variable array of params for the query
+  var params = names.slice(0, max.names).map(n=>('%' + n + '%'));
+
+  // execute query
+  db.all( sql, params, cb );
+};
