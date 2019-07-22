1143413e4b075d312bc2643b680b5a21c09e6c80
Luca <luca.saggese@gmail.com>
2019-07-22 14:28:43 +0200

    Create admin.js

---
+++ cmd/admin.js
@@ -0,0 +1,32 @@
+var admin = require('../api/admin'),
+    pretty = require('../lib/pretty');
+
+// help text
+if( process.argv.length !== 5  ){
+  console.error('invalid args.');
+  console.error('usage:  {streetdb} {wofdb} {lat} {lon}');
+  console.error('example: node cmd/admin street.db wof.db "-41.288788" "174.766843"');
+  process.exit(1);
+}
+
+var conn = admin( process.argv[2], process.argv[3] );
+
+var point = {
+  lat: parseFloat( process.argv[4] ),
+  lon: parseFloat( process.argv[5] )
+};
+
+conn.query( point,  function( err, res ){
+
+  if( !res ){
+    return console.error( '0 results found' );
+  }
+    
+  // only return 10 results
+  res = res.slice(0,10);
+
+  console.log(res);
+
+});
+
+conn.close();
