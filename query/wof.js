d3bae7780ccf3b56a26dc3815bfa21f6ac41cd6e
Luca <luca.saggese@gmail.com>
2019-07-22 16:23:02 +0200

    Update wof.js

--- query/wof.js
+++ query/wof.js
@@ -63,7 +63,7 @@ module.exports = function( db, point, cb ){
           console.log({"type":"Point","coordinates":[point.lon,point.lat]},
                  {"type":"Polygon", "coordinates":geojson.geometry})
           var isIn = gju.pointInPolygon({"type":"Point","coordinates":[point.lon,point.lat]},
-                 {"type":"Polygon", "coordinates":geojson.geometry});
+                 {"type":"Polygon", "coordinates":geojson.geometry[0]});
           console.log(isIn);
           return isIn
         }); 
